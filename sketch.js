2faed7dd0ab3d569f0c1bd7ecce7dd76ac98eaf0
Daniel Fenjves <dfenjves@users.noreply.github.com>
2016-05-19 10:46:52 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,18 +2,11 @@
 // The setup function runs once
 function setup() {
   
-  createCanvas(800, 600);
-  stroke(0); 
-  fill(150);
+  //createCanvas(800, 600);
+  //background(0);
 }
 
 //The draw function runs many times per second. The default rate is 60 frames per second!
 function draw() {
-  background(0);
-  rect(50,50,75,100);
-
-  var c = color(255, 204, 0);  // Define color 'c'
-  fill(c);  // Use color variable 'c' as fill color
-  noStroke();  // Don't draw a stroke around shapes
-  rect(30, 20, 55, 55);  // Draw rectangle
+ 
 }
